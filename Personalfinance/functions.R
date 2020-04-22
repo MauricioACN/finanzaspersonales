b910a7f612bbf57a1d50395f1e846de71b1eb080
MauricioACN <alejandrocanomn@gmail.com>
2020-04-21 22:09:18 -0500

    Actualización de las funciones de carga y guardado por medio de la conexión dropbox.

--- Personalfinance/functions.R
+++ Personalfinance/functions.R
@@ -1,15 +1,35 @@
 
+
 saveData <- function(data) {
-  data <- as.data.frame(t(data))
-  if (exists("responses")) {
-    responses <<- rbind(responses, data)
-  } else {
-    responses <<- data
-  }
+  
+  # data <- as.data.frame(t(data))
+  # 
+  #   if(exists("responses")){
+  #     
+  #     data <<-rbind(responses,data)
+  #   }
+  # else{
+  #   
+  #   data <<- data
+  # }
+
+  #data <- rbind(base_cargada,data)
+  file_name <- sprintf("registros.csv")
+  file_path <- file.path(tempdir(), file_name)
+  write.csv(data, file_path, row.names = FALSE, quote = TRUE)
+  drop_upload(file_path, path = "datos_app")
 }
 
 loadData <- function() {
-  if (exists("responses")) {
-    responses
-  }
+  
+  # Read all the files into a list
+  filesInfo <- drop_dir("datos_app")
+  filePaths <- filesInfo$path_display
+  data <- lapply(filePaths, drop_read_csv, stringsAsFactors = FALSE)
+  # Concatenate all data together into one data.frame
+  data <- do.call(rbind, data)
+  data
 }
+
+
+
