e72baaa594eac3da436bac9d210c368fdd410816
MauricioACN <alejandrocanomn@gmail.com>
2020-04-22 22:47:52 -0500

    Se realizó la actualización de los paquetes necesarios para la app.

--- Personalfinance/global.R
+++ Personalfinance/global.R
@@ -1,22 +1,14 @@
 #### library ----------------------------
 
-library(tm)
-library(wordcloud2)
 library(tidyverse)
 library(zoo)
-library(factoextra)
-library(lubridate)
 library(DT)
 library(ggplot2)
 library(ggthemes)
-library(syuzhet)
-library(scales)
-library(reshape2)
 library(plotly)
 library(shiny)
 library(shinythemes)
-
-#### user list to save personal finances ----------------------------
+library(rdrop2)
 
 # categorias
 categorias = read.csv("categorias.csv", header = T, sep = ",")
@@ -24,5 +16,5 @@ categorias = read.csv("categorias.csv", header = T, sep = ",")
 
 #### library ----------------------------
 
-fields <- c("Fecha",colnames(categorias),"Valor")
+fields <- c("nombre","fecha","modelo","rubro","categoria","subcategoria","valor")
 
